05c981f061304332c928826dd5b675a0bd1a7d6c
akuadane <akuadane1205@gmail.com>
2020-05-22 10:29:33 +0300

    Map added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 main() => runApp(MainWindow());
-
 
 class MainWindow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MapRoute(),
+    );
+  }
+}
+
+class MapRoute extends StatefulWidget {
+  @override
+  _MapRouteState createState() => _MapRouteState();
+}
+
+class _MapRouteState extends State<MapRoute> {
+  Position _position;
+
+  _getPostion() async {
+    _position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getPostion();
+    return Scaffold(
+      body: _getMap(context),
+    );
+  }
+
+  Widget _getMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(_position.latitude, _position.longitude),
+          zoom: 18,
+        ),
+      ),
+    );
   }
 }
